68e84fef995e4d523a1dca6eb22fe61958b504b3
ma <ma@HaPC>
2018-03-26 13:27:19 +0800

    修改mat util

--- src/com/lcf/util/ReadMatUtil.java
+++ src/com/lcf/util/ReadMatUtil.java
@@ -3,14 +3,28 @@ package com.lcf.util;
 import com.jmatio.io.MatFileReader;
 import com.jmatio.types.MLArray;
 import com.jmatio.types.MLDouble;
+import com.jmatio.types.MLStructure;
 
-public class ReadMatUtil { 
-	public static double[][] ReadMat(String matPath, String matrixName) throws Exception {
+public class ReadMatUtil {
+	
+	public static double[][] getMatRes(MLStructure mat) {
+		MLArray res = mat.getField("res");
+		MLDouble d_res = (MLDouble) res;
+		double [][] resArray = d_res.getArray();
+		return resArray;
+	}
+	
+	public static double[][] getMatFps(MLStructure mat) {
+		MLArray fps = mat.getField("fps");
+		MLDouble d_fps = (MLDouble) fps;
+		double [][] fpsArray = d_fps.getArray();
+		return fpsArray;
+	}
+	
+	public static MLStructure ReadMat(String matPath) throws Exception {
 		MatFileReader read = new MatFileReader(matPath);
-		MLArray mlArray = read.getMLArray(matrixName);
-		MLDouble d = (MLDouble) mlArray;
-		double[][] matrix = (d.getArray());
-		
-		return matrix;
+		MLArray mlArray = read.getMLArray("res");
+		MLStructure ms = (MLStructure) mlArray;
+		return ms;
 	}
 }
