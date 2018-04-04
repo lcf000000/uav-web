768a6f1780013b3591757d12c836d2c8973b5dc3
ma <ma@HaPC>
2018-04-04 22:56:06 +0800

    更新发送邮件模块，配置发件邮箱

--- src/com/lcf/util/SendEmailUtil.java
+++ src/com/lcf/util/SendEmailUtil.java
@@ -12,19 +12,24 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class SendEmailUtil {
+	
 	public static void sendEmail(String userEmail) throws Exception {
 		Properties props = new Properties();
+		
+		String emailAddress="tju.drone.vision@gmail.com";
+		String password="VisDrones2018";
 		// 获取邮箱smtp服务器的地址
-		props.setProperty("mail.host", "smtp.163.com");
-		// 是否进行权限验证
-		props.setProperty("mail.smtp.aauth", "true");
+		props.setProperty("mail.host", "smtp.gmail.com");
+        //是否进行权限验证。
+        props.setProperty("mail.smtp.ssl.enable", "true");
+        props.setProperty("mail.smtp.auth", "true");
 		
 		// 权限
 		Authenticator authenticator = new Authenticator() {
 			@Override
             public PasswordAuthentication getPasswordAuthentication() {
                 //填写自己的163邮箱的登录帐号和授权密码，授权密码的获取，在后面会进行讲解。
-                return new PasswordAuthentication("fcbar8@163.com","");
+                return new PasswordAuthentication(emailAddress,password);
             }  
 		};
 		
@@ -44,7 +49,7 @@ public class SendEmailUtil {
         // 创建消息
         Message message = new MimeMessage(session);
         // 发件人      
-        message.setFrom(new InternetAddress("fcbar8@163.com"));
+        message.setFrom(new InternetAddress(emailAddress));
         /**
          *  收件人
          *         第一个参数：
@@ -62,7 +67,7 @@ public class SendEmailUtil {
         // 主题（标题）
         message.setSubject("VisDrone 2018 Challenge Submission Confirmation");
         // 正文
-        String str = "Dear: <br/>" +
+        String str = "Dear user: <br/>" +
                 "Your result was submitted successfully to VisDrone 2018." +
                 "<br/>" +
                 "<br/>" +
