8be418edc672b3ce2da1e52f511abce244839998
ma <ma@HaPC>
2018-04-18 12:54:18 +0800

    完善邮箱验证模块

--- src/com/lcf/util/GenerateLinkUtil.java
+++ src/com/lcf/util/GenerateLinkUtil.java
@@ -6,17 +6,17 @@ public class GenerateLinkUtil {
 	
 	private static final String CHECK_CODE = "checkCode";
 	
-	public static String generateCheckcode(String userName, String randomCode) {
+	public static String generateCheckcode(String userName, String randomCode) throws Exception {
 		return MD5.md5Link(userName + ":" + randomCode);
 	}
 	
-	public static String generateActiveLink(String id, String userName, String randomCode) {
+	public static String generateActiveLink(String id, String userName, String randomCode) throws Exception {
 		//return "http://localhost:8080/UAV-Data/views/activateAccount?id="
 		return "http://www.aiskyeye.com/views/activateAccount?id="
 				+ id + "&" + CHECK_CODE + "=" + generateCheckcode(userName, randomCode);
 	}
 	
-	public static boolean verifyCheckCode(String userName, String randomCode, String checkCode) {
+	public static boolean verifyCheckCode(String userName, String randomCode, String checkCode) throws Exception {
 		return generateCheckcode(userName, randomCode).equals(checkCode);
 	}
 }
